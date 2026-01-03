889adc6ce9a5626a70ef67599fe9b7d7ba583d83
Muhammad Umais <umaisimran96@gmail.com>
2026-01-03 12:02:55 +0500

    Nav Bar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from "./componenets/NavBar.jsx";
+
+const App = () => {
+    return (
+        <main>
+            <NavBar />
+        </main>
+    )
+}
+
+export default App
