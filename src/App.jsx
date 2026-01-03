9f349134a547c85cbdde22dbab08778a0a5b741a
Muhammad Umais <umaisimran96@gmail.com>
2026-01-03 12:11:24 +0500

    Hero Section with video

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from "./componenets/NavBar.jsx";
+import Hero from "./componenets/Hero.jsx";
 
 const App = () => {
     return (
         <main>
             <NavBar />
+            <Hero/>
         </main>
     )
 }
