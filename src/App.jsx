e8769f9df81a2ede3cb9715120d7a0db898aba98
Muhammad Umais <umaisimran96@gmail.com>
2026-01-05 17:49:33 +0500

    landing page clone with 3d models

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
 import NavBar from "./componenets/NavBar.jsx";
 import Hero from "./componenets/Hero.jsx";
+import ProductViewer from "./componenets/ProductViewer.jsx";
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/all";
+import Showcase from "./componenets/Showcase.jsx";
+import Performance from "./componenets/Performance.jsx";
+import Features from "./componenets/Features.jsx";
+import Highlights from "./componenets/Highlights.jsx";
+import Footer from "./componenets/Footer.jsx";
+
+gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
     return (
         <main>
             <NavBar />
-            <Hero/>
+            <Hero />
+            <ProductViewer />
+            <Showcase />
+            <Performance />
+            <Features />
+            <Highlights />
+            <Footer />
         </main>
     )
 }
